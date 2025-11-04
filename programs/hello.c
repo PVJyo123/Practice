aa4986105cdb70f23c48b83e72beae37b033c05b
PVJyo123 <jyothipokuri01@gmail.com>
2025-11-04 12:13:43 +0530

    Update hello.c

--- programs/hello.c
+++ programs/hello.c
@@ -3,3 +3,19 @@ int main() {
     printf("Hello from C!\n");
     return 0;
 }
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello from C++!" << endl;
+    return 0;
+}
+
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello from Java!");
+    }
+}
