7ce573c0344eea661e36010ae0251791661eef6c
PVJyo123 <jyothipokuri01@gmail.com>
2025-11-04 12:28:04 +0530

    Update hello.c

--- programs/hello.c
+++ programs/hello.c
@@ -1,21 +1,7 @@
+'''
 #include <stdio.h>
 int main() {
     printf("Hello from C!\n");
     return 0;
 }
-
-
-#include <iostream>
-using namespace std;
-
-int main() {
-    cout << "Hello from C++!" << endl;
-    return 0;
-}
-
-
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello from Java!");
-    }
-}
+'''
